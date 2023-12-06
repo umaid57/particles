217023da9cd36e5abcb52dec7cfde317b7ba2cff
umaid <leafshade56@gmail.com>
2023-12-06 11:34:22 -0800

    Umaid and Elijah

--- Particle.h
+++ Particle.h
@@ -5,7 +5,7 @@
 const float MEMBER_PI = 3.1415926535897932384626433;
 const float G = 1000;      //Gravity
 const float TTL = 5.0;  //Time To Live
-const float SCALE = 0.999;
+const float SCALE = 0.999; //shrink
 
 using namespace Matrices;
 using namespace sf;
